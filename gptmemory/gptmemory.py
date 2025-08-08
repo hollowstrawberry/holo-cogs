fab64337cd2424f4ca21ce84a134f2d941b7c3d2
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-08-07 23:17:16 -0400

    Update gptmemory.py

--- gptmemory/gptmemory.py
+++ gptmemory/gptmemory.py
@@ -235,13 +235,13 @@ class GptMemory(GptMemoryBase):
                     reasoning_effort=await self.config.guild(ctx.guild).effort_responder() if "gpt-5" in model else NotGiven()
                 )
 
-            if response.choices and response.choices[0].message and response.choices[0].message.content:
-                completion = response.choices[0].message.content
+            log.info(response)
+
+            completion = response.choices[0].message.content
+            if completion:
                 log.info(f"{completion=}")
                 reply_content = RESPONSE_CLEANUP_PATTERN.sub("", completion)
                 await chunk_and_send(ctx, reply_content)
-            else:
-                log.warning("Empty completion", response)
 
         response_message = {
             "role": "assistant",
