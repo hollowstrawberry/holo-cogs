37ecf91d8de75291566cc2a2e2dc09777a39c852
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-08-24 17:21:47 -0400

    tweak message

--- gptmemory/functions/stable_diffusion.py
+++ gptmemory/functions/stable_diffusion.py
@@ -70,6 +70,7 @@ class StableDiffusionFunctionCall(FunctionCallBase):
             messages.append(quoted)
         for message in messages:
             for attachment in message.attachments:
+                log.info(filename)
                 if attachment.filename == filename and self.ctx.guild:
                     return (message.author.id == self.ctx.guild.me.id, message)
         return (False, None)
@@ -148,7 +149,7 @@ class StableDiffusionFunctionCall(FunctionCallBase):
                 height=height
             )
 
-        message_content = f"Requested by {self.ctx.author} at {self.ctx.message.jump_url}"
+        message_content = f"Result of {self.ctx.message.jump_url}\nRequested by {self.ctx.author} at "
         task = aimage.generate_image(self.ctx, params=params, message_content=message_content) # type: ignore
         asyncio.create_task(task)
 
