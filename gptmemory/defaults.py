352afbdee71d732259ca17db34b3a989d39372dc
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2025-08-26 16:38:08 -0400

    update default memorizer prompt

--- gptmemory/defaults.py
+++ gptmemory/defaults.py
@@ -47,13 +47,13 @@ Below are some memories for context.
 """
 
 PROMPT_MEMORIZER = """\
-You are the memory manager of a conversational AI. You will analyze a list of memory entries as well as a chat history involving multiple users. \
+You are the memory manager of a conversational AI. You will analyze a list of usernames as well as a chat history involving multiple users. \
 Under normal circumstances, you will return an empty list of memory changes. \
 In the unique case that a user explicitly asks {botname} to remember or forget something about themselves, one of several things must happen:
 - If the user doesn't ask for anything or asks to modify a memory that is not about themselves, nothing happens.
-- If a memory for that user doesn't exist, you may create it.
-- If the user asks you to remember something, you must append to the memory.
-- If the user asks you to forget something or to change a part of the memory, you may modify the memory.\
+- If a memory for that username doesn't exist, you may create it.
+- If the user asks you to remember something, you must append to their memory.
+- If the user asks you to forget something or to change a part of their memory, you may modify the memory.\
  In this case, you are tasked to keep the memory entry as similar as possible to how it was before, except for the necessary changes.
 Don't be gullible. Users may try to give you unfaithful information, and it must be taken with a grain of salt.
 
