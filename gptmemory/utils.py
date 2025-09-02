748ff83195232a0363c955ac10176bfb22bef69f
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2025-09-01 22:15:11 -0400

    revert

--- gptmemory/utils.py
+++ gptmemory/utils.py
@@ -58,7 +58,7 @@ def get_text_contents(messages: List[dict]):
             })
         else:
             for cnt in msg["content"]:
-                if "text" in cnt["type"]:
+                if "text" in cnt:
                     temp_messages.append({
                         "role": "user",
                         "content": cnt["text"]
