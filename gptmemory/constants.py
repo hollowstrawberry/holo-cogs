a540c9d3c0490e0a2f134eb103e5fedce65a8c1b
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-01-02 02:38:36 -0300

    tweak response cleanup pattern

--- gptmemory/constants.py
+++ gptmemory/constants.py
@@ -5,12 +5,12 @@ from discord.utils import DISCORD_EPOCH
 MAX_MESSAGE_LENGTH = 1950
 IMAGE_EXTENSIONS = (".png", ".jpg", ".jpeg", ".webp", ".bmp", ".gif")
 
-# ((^|\n)(\[[^[:\]]*:[^[:\]]*\]\s?)+)    Author system texts such as [Username: Crabot]
+# (^(\[[^[:\]]*:[^[:\]]*\]\s?)+)         Author system texts such as [Username: Crabot]
 # (\[\[\[[\s\S]+\]\]\])                  Multiline system texts inside [[[ triple brackets ]]]
 # (\[\[.+\]\])                           Single-line system texts inside [[ double brackets ]]
 # (-# (Requested|Revised).+)             Bot actions that are automated and the AI likes to repeat
-# ({\s*"ai_?generated":[^}]+})           Gemini boilerplate text for images
-RESPONSE_CLEANUP_PATTERN = re.compile(r'((^(\[[^[:\]]*:[^[:\]]*\]\s?)+)|(\[\[\[[\s\S]+\]\]\])|(\[\[.+\]\])|(-# (Requested|Revised).+)|({\s*"ai_?generated":[^}]+}))', re.MULTILINE)
+# ({\s*"(ai_?generated|url)":[^}]+}))    Gemini boilerplate text for images
+RESPONSE_CLEANUP_PATTERN = re.compile(r'((^(\[[^[:\]]*:[^[:\]]*\]\s?)+)|(\[\[\[[\s\S]+\]\]\])|(\[\[.+\]\])|(-# (Requested|Revised).+)|({\s*"(ai_generated|url)":[^}]+}))', re.MULTILINE)
 
 INCOMPLETE_EMOTE_PATTERN = re.compile(r"<?(a?:\w{2,}:\d{17,19})>?")
 FARENHEIT_PATTERN = re.compile(r"(-?\d+)\s?°[fF]")
