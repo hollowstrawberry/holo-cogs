725ef0719cefa0e2d23f49ec94afdd0aa2bf40b2
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-01-04 22:59:44 -0300

    add indicator of web search

--- gptmemory/functions/search_web.py
+++ gptmemory/functions/search_web.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 from openai import NotGiven
 
 from gptmemory.schema import ToolCall, Function, Parameters
@@ -23,6 +24,9 @@ class AgenticSearchFunctionCall(FunctionCallBase):
 
     async def run(self, arguments: dict) -> str:
         assert self.ctx.guild and self.cog.openai_client and self.cog.openrouter_client
+        if self.ctx.bot_permissions.add_reactions:
+            asyncio.create_task(self.ctx.message.add_reaction("🌐"))
+
         model = await self.cog.config.guild(self.ctx.guild).model_responder()
         if "/" in model:  # openrouter
             response = await self.cog.openrouter_client.responses.create(
@@ -47,6 +51,7 @@ class AgenticSearchFunctionCall(FunctionCallBase):
                 tools=[{"type": "web_search"}],  # type: ignore
                 input=arguments["query"]
             )
+            
         assert response.usage and response.output_text
         log.info(f"WebSearchResult(input_tokens={response.usage.input_tokens}, output_tokens={response.usage.output_tokens})")
         return response.output_text
