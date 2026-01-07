0a48e0f29b60166f50c891b293c8d0f979ef77fd
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-01-07 17:34:03 -0300

    Fix regex

--- gptmemory/utils.py
+++ gptmemory/utils.py
@@ -136,7 +136,7 @@ def format_arcenciel_model(data: dict) -> str:
         if i == 0 and data['type'] == "LORA":
             versions_info += " [Activation tags:]"
             filename = version.get('originalName') or version.get('fileName')
-            filename = re.sub(r"[^a-zA-Z0-9._- ]", "_", filename).replace(".safetensors", "") # sanitize
+            filename = re.sub(r"[^a-zA-Z0-9. _-]", "_", filename).replace(".safetensors", "") # sanitize
             lora = f"<lora:{filename}:1>" if filename else ""
             if version.get('activationTags', []):
                 for tags in version['activationTags']:
