06ec553a4ab9ff2451fe51460ecc09b1ec729779
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-03-30 18:51:00 -0300

    WHYY

--- aimage/aimage.py
+++ aimage/aimage.py
@@ -180,10 +180,8 @@ class AImage(AImageCommands):
                 mask_paths = []
                 masks = build_split_masks(payload["width"], payload["height"], params.regions.split_percent, params.regions.split_type)
                 for filename, data in masks:
-                    log.info(len(data))
                     mask_paths.append(await self.api.upload_image(data, filename or "image.png"))
-                    log.info(len(data))
-                    file = discord.File(data, "fuck.png")
+                    file = discord.File(BytesIO(data), "fuck.png")
                     await context.channel.send(content=hashlib.sha256(data).digest().decode(), file=file)
                 for i, path in enumerate(mask_paths):
                     payload["attentionCouple"]["regions"][i]["maskPath"] = path
