66ce1daed7b6025c342d106c7d49a836b90841e2
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-06 00:44:45 -0400

    Update hi_res.py

--- aimage/views/hi_res.py
+++ aimage/views/hi_res.py
@@ -98,7 +98,6 @@ class HiresModal(ui.Modal):
         await self.generate_image(interaction, payload=self.payload, callback=self.edit_callback(), message_content=message_content)
 
     async def edit_callback(self):
-        await asyncio.sleep(0.1)
         assert self.parent_interaction.message
         self.parent_button.disabled = False
         if not self.parent_view.is_finished():
