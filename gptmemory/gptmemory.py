0b77c8d29be0033153036e6907e6a09e2b6c4c18
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-09 21:36:24 -0400

    Update gptmemory.py

--- gptmemory/gptmemory.py
+++ gptmemory/gptmemory.py
@@ -216,8 +216,10 @@ class GptMemory(GptMemoryCommands):
                         log.error(f"Response got censored {constants.CENSORED_RETRIES} times, aborting.")
                         await ctx.message.add_reaction(await self.config.blocked_emoji())
                     else:
-                        log.warning(f"Response got censored, trying...")
-                        messages = messages[1:] + [utils.make_dummy_message(ctx)]
+                        log.warning(f"Response got censored, retrying...")
+                        messages.append(utils.make_dummy_message(ctx))
+                        if len(messages) - i > 2:
+                            messages = messages[1:]
                         await asyncio.sleep(2)
                 except Exception:
                     raise
