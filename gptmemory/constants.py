c99181b5b3430c9f8acea8f2079976b9136ad1d6
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-16 20:46:08 -0400

    Cleanup patterns for trailing or duplicated chat_message

--- gptmemory/constants.py
+++ gptmemory/constants.py
@@ -13,10 +13,12 @@ TOKEN_ENCODING = "o200k_base"
 PROMPT_TYPES = ("responder", "recaller", "memorizer", "autoresponder")
 
 RESPONSE_CLEANUP_PATTERNS = OrderedDict({
-    "XML content":        (re.compile(r"^.*<content>(.*?)</content>.*$", re.DOTALL | re.IGNORECASE), r"\1"),
+    "trailing message":   (re.compile(r"(.{10,}?)\s*<chat_message.*</chat_message>", re.DOTALL | re.IGNORECASE), r"\1"),
+    "double message":     (re.compile(r"<chat_message(?: [^>]+)?>(.*?)</chat_message>\s*<chat_message.*</chat_message>", re.DOTALL | re.IGNORECASE), r"\1"),
+    "message content":    (re.compile(r"^.*<content>(.*?)</content>.*$", re.DOTALL | re.IGNORECASE), r"\1"),
     "XML objects":        (re.compile(r"<(quote|linked_message|message_link|embeds?|attachments?|images?|stickers?|buttons?|reactions?|poll)( [^>]+)?>.*?</\1>", re.DOTALL | re.IGNORECASE), ""),
     "Automated actions":  (re.compile(r"^\s*-?\s*#\s*(Request|Revise|Reroll|Result|Upscale|Change|Variation).+", re.MULTILINE | re.IGNORECASE), ""),
-    "Image objects":      (re.compile(r"{[^}]*?(image|file|action)[^}]*?}(?!\s*```)", re.IGNORECASE), ""),
+    "JSON objects":       (re.compile(r"{[^}]*?(image|file|action)[^}]*?}(?!\s*```)", re.IGNORECASE), ""),
     "Leftover symbol":    (re.compile(r"""\n[}'"\s\-]+$"""), ""),
     "Server emote":       (re.compile(r"`?(?:&lt;|<)?(a?:\w+:\d{17,19})(?:&gt;|>)?`?"), r"<\1>"),
     "Em dash":            (re.compile(r"(?<=\w)—(?=\w)"), ", "),
