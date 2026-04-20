7b8315cc6a5d53061da43508b16351351e58bf67
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-20 02:06:15 -0400

    fix cleanup of multiple messages with generated image

--- gptmemory/constants.py
+++ gptmemory/constants.py
@@ -29,7 +29,7 @@ RESPONSE_CLEANUP_PATTERNS = [
     ("Em dash",           re.compile(r"(?<=\w)—(?=\w)"), ", "),
 ]
 GENERATE_IMAGE_PATTERNS = {
-    "XML object":        re.compile(r"<(\w+)(?: [^>]+)?>.*?<prompt>(.*?)<\/prompt>.*?<\/\1>", re.DOTALL | re.IGNORECASE),
+    "XML object":        re.compile(r"<(\w+)(?: [^>]+)?>(?:(?!</\1>).)*<prompt>(.*?)</prompt>(?:(?!</\1>).)*</\1>", re.DOTALL | re.IGNORECASE),
     "JSON object":       re.compile(r"""{\s*(?:["']action["'].+?)?["']prompt["']:\s*["']([^"']+)["'].*$""", re.DOTALL | re.IGNORECASE),
 }
 #INCOMPLETE_EMOTE_PATTERN = re.compile(r"`?(?:&lt;|<)?a?:(\w{3,}):\d{0,16}(?!\d)(?:&gt;|>)?`?")
