124fb515227f122103397674154c45c51854635d
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-04-29 15:59:07 -0400

    uncomment parameters

--- gptmemory/gptmemory.py
+++ gptmemory/gptmemory.py
@@ -358,14 +358,14 @@ class GptMemory(GptMemoryCommands):
             response = await self.get_client(model).chat.completions.create(
                 model=utils.clean_model(model),
                 messages=temp_messages,  # type: ignore
-                #max_tokens=NotGiven() if "gpt-5" in model else max_tokens,  # type: ignore
-                #max_completion_tokens=NotGiven() if "gpt-5" not in model else max_tokens,  # type: ignore
+                max_tokens=NotGiven() if "gpt-5" in model else max_tokens,  # type: ignore
+                max_completion_tokens=NotGiven() if "gpt-5" not in model else max_tokens,  # type: ignore
                 tools=tools_schema if can_use_tools else None,  # type: ignore
                 tool_choice="auto" if can_use_tools else "none",
-                #reasoning_effort=NotGiven() if "gpt-4" in model else effort,  # type: ignore
-                #extra_body=None if "/" not in model else {
-                #    "session_id": str(ctx.message.id),
-                #},
+                reasoning_effort=NotGiven() if "gpt-4" in model else effort,  # type: ignore
+                extra_body=None if "/" not in model else {
+                    "session_id": str(ctx.message.id),
+                },
             )
             if response is None:
                 log.error(f"OpenAI SDK returned NoneType")
