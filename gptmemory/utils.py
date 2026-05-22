3d420074e9436f42ac38368031fb2609386d3c65
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2026-05-21 20:59:43 -0400

    Update utils.py

--- gptmemory/utils.py
+++ gptmemory/utils.py
@@ -282,7 +282,9 @@ async def bot_is_typing(channel: discord.abc.Messageable):
     """Like channel.typing but doesn't stop execution if typing fails."""
     async def keep_typing():
         while True:
+            log.info("typing")
             await asyncio.wait_for(channel.typing(), timeout=2.0)
+            log.info("waiting")
             await asyncio.sleep(5.0)
             
     task = asyncio.create_task(keep_typing())
@@ -290,5 +292,5 @@ async def bot_is_typing(channel: discord.abc.Messageable):
         yield
     finally:
         task.cancel()
-        with contextlib.suppress(Exception):
+        with contextlib.suppress(asyncio.CancelledError):
             await task
