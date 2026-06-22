ce9c7eaa929fc21879875b1f3d466f4b12c0b139
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2026-06-21 23:03:05 -0400

    Create finevoice.py

---
+++ gptmemory/tools/finevoice.py
@@ -0,0 +1,78 @@
+import logging
+import asyncio
+import aiohttp
+import discord
+
+from gptmemory.schema import ToolCall, Function, Parameters
+from gptmemory.tools.base import ToolBase
+
+log = logging.getLogger("gptmemory.searchweb")
+
+
+class FinevoiceTool(ToolBase):
+    display_name = "finevoice"
+    apis = [("finevoice", "api_key")]
+    settings = {
+        "voice_emoji": "🗣️",
+        "voice": "",
+    }
+    schema = ToolCall(
+        Function(
+            name="voice",
+            description="Uses text-to-speech to send an audio in chat with your own voice.",
+            parameters=Parameters(
+                properties={
+                    "text": {
+                        "type": "string"
+                    },
+                },
+                required=["text"],
+            )))
+
+    async def run(self, arguments: dict) -> str:
+        api_key = (await self.ctx.bot.get_shared_api_tokens("finevoice")).get("api_key")
+        if not api_key:
+            log.error("finevoice api_key not found")
+            return "<error>An error occured while using voice.</error>"
+        
+        if self.ctx.bot_permissions.add_reactions:
+            emoji = self.get_setting("voice_emoji")
+            asyncio.create_task(self.ctx.message.add_reaction(emoji))
+
+        if not arguments.get("text"):
+            log.error("llm did not provide text for tts")
+            return "<error>You didn't send a text to convert into audio.</error>"
+
+        payload = {
+            "voice": self.get_setting("voice"),
+            "text": arguments["text"],
+        }
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
+        }
+
+        try:
+            async with self.cog.session.post("https://apis.finevoice.ai/v1/audio/speech-synthesis", payload=payload, headers=headers) as response:
+                response.raise_for_status()
+                data = await response.json()
+        except aiohttp.ClientError:
+            log.exception("finevoice tool: Failed to get response from endpoint.")
+            return "<error>An error occured while using voice.</error>"
+        
+        if not data or not data.get("url"):
+            log.error(f"finevoice tool: Response data does not contain necessary 'url' field. Response data:\n{data}")
+            return "<error>An error occured while using voice.</error>"
+
+        try:
+            async with self.cog.session.get(data["url"]) as response:
+                response.raise_for_status()
+                data = await response.read()
+        except aiohttp.ClientError:
+            log.exception("finevoice tool: Failed to download result.")
+            return "<error>An error occured while using voice.</error>"
+        
+        file = discord.File(data, filename=f"{self.ctx.me.display_name} speaking.mp3")
+        await self.ctx.reply(file=file, allowed_mentions=discord.AllowedMentions.none())
+
+        return "<result>You sent an audio file with your voice in chat.</result>"
